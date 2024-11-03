ac4e9f22478918c780c62f2c0236d7106aa63c5d
camblsoup <cam.mclagan@gmail.com>
2024-11-03 15:14:41 -0800

    init + windows95 themed window

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './css/App.css'
+import Window from './Window'
+
+function App() {
+
+
+  return (
+    <>
+        <Window />
+    </>
+  )
+}
+
+export default App
---
+++ src/Window.tsx
@@ -0,0 +1,35 @@
+import './css/Window.css'
+import icon from './assets/map-icon.png'
+import minimize from './assets/minimize.png'
+import maximize from './assets/maximize.png'
+import cross from './assets/cross.png'
+
+// Window Element Specification
+function Window() {
+
+    // Html to return
+    return (
+        <>
+            <div className={"window"}>
+                <div className={"top-bar"}>
+                    <div className={"title"}>
+                        <img src={icon} alt={"map icon"}/>
+                        <h1>Map</h1>
+                    </div>
+                    <div className={"window-buttons"}>
+                        <div className={"window-size-buttons"}>
+                            <button className={"window-button"}><img className={"window-button-icon"} src={minimize} alt={"minimize icon"}/></button>
+                            <button className={"window-button"}><img className={"window-button-icon"} src={maximize} alt={"maximize icon"}/></button>
+                        </div>
+                        <button className={"window-button"}><img className={"window-button-icon"} src={cross} alt={"close icon"}/></button>
+                    </div>
+                </div>
+                <div className={"body"}></div>
+                <div className={"info"}></div>
+            </div>
+        </>
+    )
+}
+
+// Specifies a default function to return when called
+export default Window
