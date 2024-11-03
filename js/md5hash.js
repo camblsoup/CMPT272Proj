0d21c596ad33a5775c06c08721ab4367a1e3166f
onioin <canyongudaitis@gmail.com>
2024-11-03 00:20:42 -0700

    md5 hash function

---
+++ js/md5hash.js
@@ -0,0 +1,30 @@
+/*
+ * MD5 HASH FUNCTION
+ * USES HASHIFY.NET API
+ */
+
+//using this function:
+//      hash("anystring").then(result => {
+//              let hashed = result["Digest"]
+//              /* Operate on the returned value */
+//          }).catch(err => console.log(err));
+//
+//@param {string} toHash - string to be hashed
+//@return {Promise} - returns a promise object
+function hash(toHash) {
+    let requestOptions = {
+        method: 'POST',
+        body: toHash,
+        redirect: 'follow'
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch("https://api.hashify.net/hash/md5/hex", requestOptions)
+            .then(response => response.json())
+            .then(result => resolve(result))
+            .catch(error => reject(error));
+    })
+
+}
+hash("helloWorld")
+    .then((result) => console.log(result["Digest"]))
